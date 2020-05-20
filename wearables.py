39bc34c50846b41edbd0f4aac0c3fe8fdccf85a9
John Peck <jcpeck@peck.net>
2020-05-20 22:52:06 +0100

    Enhanced message encoding to support eye_goto positions

--- wearables.py
+++ wearables.py
@@ -28,19 +28,26 @@ class wearables_server_t(object):
         if type(msg) is tuple:
             msg_effect = msg[0]
             if msg[1] in ['fast']:
-                elapsed = 2
+                hue_med = 2
             elif msg[1] in ['slow']:
-                elapsed = 1
+                hue_med = 1
             else:
                 pass
+            if msg[0] in ['eye_goto']:
+                elapsed = msg[2]
+                beat = msg[3]
         elif type(msg) is list:
             raise
         else:
             msg_effect = msg
-        print "TX   %-16s elapsed: %04d beat: %04d hue_med:%03d hue_dev:%03d" % (msg_effect, elapsed, beat, hue_med, hue_dev)
+
+        if self.debug:
+            print "TX   %-16s elapsed: %04d beat: %04d hue_med:%03d hue_dev:%03d" % (msg_effect, elapsed, beat, hue_med, hue_dev)
         frame = struct.pack("!I12s16sIIBB", MESSAGE, '', msg_effect, elapsed, beat, hue_med, hue_dev)
         self.s.sendto(frame, (self.mcaddr, self.port))
         self.cnt_send += 1
+        if self.debug:
+            print ('cnt_send: {}'.format(self.cnt_send))
         
         
 class wearables_client_t(object):
@@ -72,16 +79,24 @@ class wearables_client_t(object):
                  hue_dev) = \
                      struct.unpack("!I12s16sIIBB", data)
 
+                x_pos,y_pos = None,None
                 effect = effect.rstrip('\x00')
-                if elapsed in [2]:
+                if effect in ['eye_goto']:
+                    x_pos = elapsed
+                    y_pos = beat
+                
+                if hue_med in [2]:
                     effect = (effect,'fast')
-                elif elapsed in [1]:
+                elif hue_med in [1]:
                     effect = (effect,'slow')
-                elif elapsed in [0]:
+                elif hue_med in [0]:
                     pass
                 else:
-                    print ('** Unexpected elapsed: {}'.format(elapsed))
+                    print ('** Unexpected hue_med encoding: {}'.format(elapsed))
 
+                if x_pos is not None and y_pos is not None:
+                    effect = (effect[0], effect[1], x_pos,y_pos)
+                    
                 msg_rec = {
     	            'msgcode' : msgcode,
                     'reserved' : reserved,
@@ -96,9 +111,12 @@ class wearables_client_t(object):
 
                 msgs.append(msg_rec)
                 self.cnt_recv += 1
+                if self.debug:
+                    print ('cnt_recv: {}'.format(self.cnt_recv))
                 
-#    	        print "RX %s:%s   %-16s elapsed: %04d beat: %04d hue_med: %03d hue_dev: %03d" % (addr[0], addr[1], effect.rstrip('\0'), elapsed, beat, hue_med, hue_dev)
-    	        print "RX %s:%s   %-16s elapsed: %04d beat: %04d hue_med: %03d hue_dev: %03d" % (addr[0], addr[1], effect, elapsed, beat, hue_med, hue_dev)
+                if self.debug:
+    	            print "RX %s:%s   %-16s elapsed: %04d beat: %04d hue_med: %03d hue_dev: %03d" % (addr[0], addr[1], effect, elapsed, beat, hue_med, hue_dev)
+#    	             print "RX %s:%s   %-16s elapsed: %04d beat: %04d hue_med: %03d hue_dev: %03d" % (addr[0], addr[1], effect.rstrip('\0'), elapsed, beat, hue_med, hue_dev)
             except socket.error, e:
                 if e.args[0] == errno.EWOULDBLOCK:
                     break
